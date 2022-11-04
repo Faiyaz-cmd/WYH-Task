99ae52df2ba47bd31c2b916e82833965aec41338
Faiyaz <faiyazshaikh3537@gmail.com>
2022-11-04 20:01:37 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { state } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Task';
+  selectedDay: string = '';
+  stateobject:any = {
+    "Maharashtra": ['Beed', 'stara', 'pune'],
+    "UP": ['Varanasi', 'Lucknnow', 'noida'],
+    "MP": ['bhopa', 'itarsi', 'khandwa']
+  }
+ list: any;
+  selectChangeHandler (event: any) {
+    const state = event.target.value;
+    // console.log(state);
+    // console.log(this.stateobject[event.target.value]);
+    this.list = [...(this.stateobject[event.target.value])]
+
+  }
+
+
+
+
 }
